47b43f7ffec83161c8de04b9c285b3078e82262e
Justinian <nicky_torvald_dk@protonmail.com>
2022-06-18 19:45:14 +0200

    added counter, timer and images for the dice

--- src/components/die/Die.jsx
+++ src/components/die/Die.jsx
@@ -1,14 +1,36 @@
 /* eslint-disable react/prop-types */
 import './Die.css'
+import img1 from '../../assets/die1.png'
+import img2 from '../../assets/die2.png'
+import img3 from '../../assets/die3.png'
+import img4 from '../../assets/die4.png'
+import img5 from '../../assets/die5.png'
+import img6 from '../../assets/die6.png'
 
 function Die(props) {
+  let image
+  if (props.value === 1) {
+    image = img1
+  } else if (props.value === 2) {
+    image = img2
+  } else if (props.value === 3) {
+    image = img3
+  } else if (props.value === 4) {
+    image = img4
+  } else if (props.value === 5) {
+    image = img5
+  } else if (props.value === 6) {
+    image = img6
+  }
+
   return (
     <div
       className='die'
       style={{ backgroundColor: props.isHeld ? '#59E391' : 'antiquewhite' }}
       onClick={() => props.check(props.id)}
     >
-      <h1>{props.value}</h1>
+      {/* <h1>{props.value}</h1> */}
+      <img src={image} alt='the die image' className='die-image' />
     </div>
   )
 }
--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,12 +1,16 @@
 import './Game.css'
 import Die from '../die/Die'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Confetti from 'react-confetti'
 
 function Game() {
   // eslint-disable-next-line no-unused-vars
   const [dice, setDice] = useState(getRandomArray)
   const [hasWon, setHasWon] = useState(false)
+  const [counter, setCounter] = useState(0)
+  const [timer, setTimer] = useState(0)
+
+  const temp = useRef(null)
 
   function getRandomArray() {
     let rndArray = Array.from({ length: 10 }, () =>
@@ -32,11 +36,23 @@ function Game() {
     )
   }
 
+  function startTimer() {
+    temp.current = setInterval(() => {
+      setTimer((prev) => prev + 1)
+    }, 1000)
+  }
+
   function handleClick() {
     if (hasWon) {
       setHasWon(false)
       setDice(getRandomArray())
+      setCounter(0)
+      setTimer(0)
+      startTimer()
     }
+
+    !hasWon && setCounter((prev) => prev + 1)
+
     setDice((prevDice) =>
       prevDice.map((die) => {
         let rnd = Math.ceil(Math.random() * 6)
@@ -49,10 +65,23 @@ function Game() {
     const allHeld = dice.every((die) => die.isHeld)
     const firstValue = dice[0].value
     const allSameValue = dice.every((die) => die.value === firstValue)
+
     if (allHeld && allSameValue) {
       setHasWon(true)
     }
   }, [dice])
+
+  useEffect(() => {
+    startTimer()
+    return () => clearInterval(temp.current)
+  }, [])
+
+  useEffect(() => {
+    if (hasWon) {
+      clearInterval(temp.current)
+      return
+    }
+  }, [timer])
 
   return (
     <main className='outer-container'>
@@ -78,6 +107,10 @@ function Game() {
       <button className='btn-roll' onClick={handleClick}>
         {hasWon ? 'Replay' : 'Roll'}
       </button>
+      <div className='round-timer'>
+        <p>Round: {counter}</p>
+        <p>Timer: {timer}</p>
+      </div>
     </main>
   )
 }
